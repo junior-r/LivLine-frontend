48ffcc94cd9818a1ee611d71bf27cf5d84593c78
junior31064049@gmail.com <junior31064049@gmail.com>
2025-05-13 17:20:30 -0500

    added ui to home page

--- src/components/pages/HomeNavbar.tsx
+++ src/components/pages/HomeNavbar.tsx
@@ -1,6 +1,6 @@
-import { Link, NavLink } from "react-router";
+import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
-import { Leaf, LogIn, Menu, UserRound } from "lucide-react";
+import { LogIn, Menu, UserRound } from "lucide-react";
 import { useAuthStore } from "@/store/auth/useAuthStore";
 import UserMenu from "../blocks/UserMenu";
 import {
@@ -11,6 +11,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import logo from "@/assets/images/LivLine.svg";
 
 function Navbar() {
   const user = useAuthStore((state) => state.user);
@@ -21,12 +22,16 @@ function Navbar() {
       link: "/",
     },
     {
-      name: "Quienes Somos",
-      link: "/about-us",
+      name: "Nosotros",
+      link: "#about-us",
+    },
+    {
+      name: "Equipo",
+      link: "#team",
     },
     {
       name: "Contacto",
-      link: "/contact",
+      link: "#contact",
     },
   ];
 
@@ -34,19 +39,18 @@ function Navbar() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between mx-auto">
         <Link to={"/"} className="flex items-center gap-2">
-          <Leaf className="h-6 w-6 text-green-600" />
+          <img src={logo} className="w-12 h-12" alt="Logo de la página web." />
           <span className="text-xl font-bold">LivLine</span>
         </Link>
         <nav className="hidden md:flex gap-6">
           {menuItems.map((item, idx) => (
-            <NavLink
+            <a
               key={idx}
-              to={item.link}
-              viewTransition
+              href={item.link}
               className="text-sm font-medium hover:text-primary"
             >
               {item.name}
-            </NavLink>
+            </a>
           ))}
         </nav>
         <div className="flex items-center gap-4">
@@ -100,14 +104,13 @@ function Navbar() {
                 </SheetHeader>
                 <div className="mt-6 flex flex-col space-y-4">
                   {menuItems.map((item, idx) => (
-                    <NavLink
+                    <a
                       key={idx}
-                      to={item.link}
-                      viewTransition
+                      href={item.link}
                       className="px-3 py-2 text-sm font-medium hover:text-primary"
                     >
                       {item.name}
-                    </NavLink>
+                    </a>
                   ))}
                 </div>
               </SheetContent>
